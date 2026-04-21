a47a458a08d91348fa91aed9baee2b581d3ebc69
Mub1532 <mubkhan644@gmail.com>
2026-04-21 15:38:20 +0100

    color shceme notif if first tiem

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,6 @@
 // hooks/useTheme.ts
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export function useTheme() {
   const [theme, setTheme] = useState(false);
@@ -13,8 +14,18 @@ export function useTheme() {
 
     const isDark =
       currentLocalTheme === "dark" || (!currentLocalTheme && preferDark);
+
     setTheme(isDark);
     document.documentElement.classList.toggle("dark", isDark);
+
+    if (!currentLocalTheme) {
+      setTimeout(() => {
+        toast.info(
+          `Automatic color scheme set to ${isDark ? "dark" : "light"}`,
+        );
+        toast.info("Change color scheme by clicking the icon in the top right");
+      }, 500);
+    }
   }, []);
 
   function toggleTheme() {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,18 @@ import Navigation from "@/components/navigation/index";
 import TopBar from "@/components/navigation/Top";
 import { useUser } from "@/hooks/useUser";
 import "@/styles/globals.scss";
-import { Montserrat } from "next/font/google";
-import { useRouter } from "next/router";
-import Script from "next/script";
-import ProgressBar from "nextjs-progressbar";
-import { useEffect, useState } from "react";
-import { Bounce, ToastContainer } from "react-toastify";
 import type { MyPageProps } from "@/types/props";
 import { USER_CODES } from "@/types/user";
 import { textColor } from "@/utils/classes";
 import { pages } from "@/utils/data/pages";
 import { capitaliseFirstLetter } from "@/utils/misc/caps";
 import { defaultScrollbar, joinClasses } from "@/utils/misc/classes";
+import { Montserrat } from "next/font/google";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import ProgressBar from "nextjs-progressbar";
+import { useEffect, useState } from "react";
+import { Bounce, ToastContainer } from "react-toastify";
 
 const font = Montserrat({
   subsets: ["latin"],
@@ -43,7 +43,7 @@ export default function App({ Component, pageProps }: MyPageProps) {
     if (!router.isReady) return;
     setPageName(
       currentPage?.name ??
-        capitaliseFirstLetter(router.asPath.replace("/", "")),
+      capitaliseFirstLetter(router.asPath.replace("/", "")),
     );
   }, [router.asPath, currentPage, router.isReady]);
 
@@ -63,7 +63,6 @@ export default function App({ Component, pageProps }: MyPageProps) {
           pauseOnHover
           theme="colored"
           transition={Bounce}
-          stacked
         />
         <div className="fixed h-full mb-auto w-full overflow-hidden bg-blue-50 dark:bg-slate-800 dark:text-slate-300 text-blue-500">
           <div className="h-full w-full relative overflow-hidden">
