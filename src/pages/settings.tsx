b96d90af1f1b1c4c7dedef9a9b5a3e92e1333e65
Mub1532 <mubkhan644@gmail.com>
2026-04-28 03:10:50 +0100

    setup settings page for reminders sync etc, todo: make components etc

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,13 +1,14 @@
-import moment from "moment";
-import type { RowDataPacket } from "mysql2";
-import type { GetServerSidePropsContext } from "next";
-import { type HTMLInputTypeAttribute, useEffect, useState } from "react";
 import { toDateTimeLocal } from "@/components/calendar/modal";
 import LoginButton from "@/components/misc/LoginButton";
 import type { MyPageProps } from "@/types/props";
 import { USER_CODES } from "@/types/user";
 import verifyUser from "@/utils/auth/jwt";
 import { getDBConnection } from "@/utils/database";
+import { joinClasses } from "@/utils/misc/classes";
+import moment from "moment";
+import type { RowDataPacket } from "mysql2";
+import type { GetServerSidePropsContext } from "next";
+import { type HTMLInputTypeAttribute, useEffect, useState } from "react";
 
 interface SettingsProps extends MyPageProps {
   userCreated: string;
@@ -68,67 +69,102 @@ export default function Settings({ user, userCreated }: SettingsProps) {
 
   const debouncedSave = useDebounceCallback(updateUser, 500);
 
+  const [testToggle, setToggle] = useState(false);
+
   return (
-    <div className="h-full w-full flex gap-4 px-4 flex-col">
-      <div className="flex gap-2 h-fit w-full text-slate-200">
-        <div className="rounded-md bg-blue-500 aspect-square w-24 flex items-center justify-center text-6xl font-bold">
-          {user?.username[0].toUpperCase().trimEnd()}
-        </div>
-        <div className="flex flex-col gap-1 h-full w-full justify-center">
-          <div className="text-xl font-semibold text-blue-500 dark:text-slate-300">
-            {username}
+    <div className="h-full w-full flex md:flex-row gap-2 px-1 md:px-4 flex-col overflow-x-auto overflow-y-hidden">
+      {/* first section, user settings etc */}
+      <div className="h-fit! w-full lg:mr-12">
+        <div className="flex gap-2 h-fit w-full text-slate-200">
+          <div className="rounded-md bg-blue-500 aspect-square w-24 flex items-center justify-center text-6xl font-bold">
+            {user?.username[0].toUpperCase().trimEnd()}
           </div>
-          <div className="text-md font-medium text-blue-500 dark:text-slate-300">
-            User Since:{" "}
-            <span className="font-bold">
-              {moment(userCreated).format("dddd Do MMMM [-] HH:mm")}
-            </span>
+          <div className="flex flex-col gap-1 h-full w-full justify-center">
+            <div className="text-xl font-semibold text-blue-500 dark:text-slate-300">
+              {username}
+            </div>
+            <div className="text-md font-medium text-blue-500 dark:text-slate-300">
+              User Since:{" "}
+              <span className="font-bold">
+                {moment(userCreated).format("dddd Do MMMM [-] HH:mm")}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="h-fit w-full flex flex-col gap-2">
+          <div className="text-lg font-bold text-blue-400 mt-2">
+            Change Account Settings
+          </div>
+          <FormInput
+            icon={MdEmail}
+            title="Email"
+            value={email}
+            placeholder="Change Email"
+            type="email"
+            onChange={setEmail}
+          />
+          <FormInput
+            icon={FaUser}
+            title="Username"
+            value={username}
+            placeholder="Change Username"
+            onChange={setUsername}
+          />
+          <FormInput
+            icon={FaLock}
+            title="Password"
+            type="password"
+            value={password}
+            placeholder="Change Password"
+            onChange={setPassword}
+          />
+          <div className="h-fit w-full max-w-fit flex flex-col md:flex-row gap-2">
+            <button
+              onClick={debouncedSave}
+              type="button"
+              className="w-fit h-full p-2 bg-blue-300 dark:bg-slate-600 flex items-center justify-center gap-2 font-medium text-lg rounded-md cursor-pointer hover:bg-blue-400 hover:dark:bg-slate-700 transition-all! ease-in duration-100 text-blue-800 dark:text-slate-300"
+            >
+              <FaSave className="text-2xl text-slate-100!" />
+              <div>Save Account Settings</div>
+            </button>
+            <LoginButton
+              type="logout"
+              extraClass="p-2 w-fit text-xl!"
+              extraIconClass="text-2xl dark:text-slate-300 text-slate-100"
+            />
           </div>
         </div>
       </div>
 
-      <div className="h-fit w-full sm:w-3/4 lg:w-1/2 flex flex-col gap-2">
-        <div className="text-lg font-bold text-blue-400">
-          Change Account Settings
+      {/* second section, toggle stuff */}
+      <div className="h-full w-full flex flex-col gap-4">
+        <div className="flex gap-2 flex-col">
+          <div className="text-lg font-bold text-blue-400 ">
+            Reminders & Notification Settings
+          </div>
+          <div className="h-fit w-full gap-2 flex">
+            <div>test toggle</div>
+            <button
+              onClick={() => setToggle(!testToggle)}
+              title="Toggle Theme"
+              className="w-12 h-6 rounded-full p-1 bg-slate-400 dark:bg-slate-500 relative transition-colors duration-300 ease-in cursor-pointer"
+            >
+              <div
+                id="toggle"
+                className={joinClasses(
+                  "rounded-full w-4 h-4 bg-blue-600 dark:bg-blue-300 relative pointer-events-none transition-all duration-300 ease-out",
+                  testToggle ? "ml-6" : "ml-0! mr-auto!",
+                )}
+              />
+            </button>
+          </div>
         </div>
-        <FormInput
-          icon={MdEmail}
-          title="Email"
-          value={email}
-          placeholder="Change Email"
-          type="email"
-          onChange={setEmail}
-        />
-        <FormInput
-          icon={FaUser}
-          title="Username"
-          value={username}
-          placeholder="Change Username"
-          onChange={setUsername}
-        />
-        <FormInput
-          icon={FaLock}
-          title="Password"
-          type="password"
-          value={password}
-          placeholder="Change Password"
-          onChange={setPassword}
-        />
-        <div className="h-fit w-full max-w-fit flex flex-col md:flex-row gap-2">
-          <button
-            onClick={debouncedSave}
-            type="button"
-            className="w-fit h-full p-2 bg-blue-300 dark:bg-slate-600 flex items-center justify-center gap-2 font-medium text-lg rounded-md cursor-pointer hover:bg-blue-400 hover:dark:bg-slate-700 transition-all! ease-in duration-100 text-blue-800 dark:text-slate-300"
-          >
-            <FaSave className="text-2xl text-slate-100!" />
-            <div>Save Account Settings</div>
-          </button>
-          <LoginButton
-            type="logout"
-            extraClass="p-2 w-fit text-xl!"
-            extraIconClass="text-2xl dark:text-slate-300 text-slate-100"
-          />
+
+        <div className="text-lg font-bold text-blue-400 mt-2">
+          Sync Settings
         </div>
+
+        <div className="text-lg font-bold text-blue-400 mt-2">AI Settings</div>
       </div>
     </div>
   );
